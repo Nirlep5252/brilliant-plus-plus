7432654397aeee3cbea69ba7b293b82fb714fec1
nirlep5252 <hello@nirlep.dev>
2024-07-19 23:18:18 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const inter = Inter({
 
 export const metadata = {
   title: "Brilliant++",
-  description: "~ Team Formality",
+  description: "",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
