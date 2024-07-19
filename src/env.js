bee88a60fa49809ef4a891a0c89bf7646e88f011
nirlep5252 <hello@nirlep.dev>
2024-07-19 22:38:59 +0530

    feat: added email magic link login

--- src/env.js
+++ src/env.js
@@ -30,6 +30,12 @@ export const env = createEnv({
     CLOUDINARY_URL: z.string(),
     API_SECRET: z.string(),
     GEMINI_API_KEY: z.string(),
+
+    EMAIL_SERVER_HOST: z.string(),
+    EMAIL_SERVER_PORT: z.string(),
+    EMAIL_SERVER_USER: z.string(),
+    EMAIL_SERVER_PASSWORD: z.string(),
+    EMAIL_FROM: z.string(),
   },
 
   /**
@@ -58,6 +64,11 @@ export const env = createEnv({
     CLOUDINARY_URL: process.env.CLOUDINARY_URL,
     API_SECRET: process.env.API_SECRET,
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+    EMAIL_SERVER_HOST: process.env.EMAIL_SERVER_HOST,
+    EMAIL_SERVER_PORT: process.env.EMAIL_SERVER_PORT,
+    EMAIL_SERVER_USER: process.env.EMAIL_SERVER_USER,
+    EMAIL_SERVER_PASSWORD: process.env.EMAIL_SERVER_PASSWORD,
+    EMAIL_FROM: process.env.EMAIL_FROM,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -7,6 +7,7 @@ import {
 } from "next-auth";
 import { type Adapter } from "next-auth/adapters";
 import Google from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
@@ -54,6 +55,17 @@ export const authOptions: NextAuthOptions = {
     Google({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+    EmailProvider({
+      server: {
+        host: env.EMAIL_SERVER_HOST,
+        port: env.EMAIL_SERVER_PORT,
+        auth: {
+          user: env.EMAIL_SERVER_USER,
+          pass: env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: env.EMAIL_FROM,
     }),
     /**
      * ...add more providers here.
